5d234679378542c554c79b7a7dd29f5a62f3fe25
sravanimedidhi <sravanimedidhi858@gmail.com>
2025-12-27 11:08:07 +0530

    update

--- palindrome.py
+++ palindrome.py
@@ -54,3 +54,45 @@ def is_palindrome(s):
     return s == s[::-1]
 
 print(is_palindrome("madam"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+s = input("Enter a string: ")
+
+vowels = "aeiouAEIOU"
+
+for char in s:
+    if char in vowels:
+        print(char)
+
+
+
+
+
+
+
+
+
+s = input("Enter a string: ")
+
+vowels = "aeiouAEIOU"
+count = 0
+
+for char in s:
+    if char in vowels:
+        count += 1
+
+print("Number of vowels:", count)
+
